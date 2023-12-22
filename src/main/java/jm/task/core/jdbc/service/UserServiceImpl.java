708ecb22bb34046afd6676f1e848055d889f59cf
Vasiliy <exs777@mail.ru>
2023-12-22 11:46:42 +0300

    Исправление1

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+
 import java.sql.DatabaseMetaData;
 import java.sql.*;
 import java.util.ArrayList;
@@ -10,34 +11,34 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    public void createUsersTable() {
-        UserDaoJDBCImpl userDaoJDBC=new UserDaoJDBCImpl();
-        userDaoJDBC.createUsersTable();
-    }
+public void createUsersTable() {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    userDaoJDBC.createUsersTable();
+}
 
-    public void dropUsersTable() {
-        UserDaoJDBCImpl userDaoJDBC=new UserDaoJDBCImpl();
-        userDaoJDBC.dropUsersTable();
-    }
+public void dropUsersTable() {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    userDaoJDBC.dropUsersTable();
+}
 
-    public void saveUser(String name, String lastName, byte age) {
-        UserDaoJDBCImpl userDaoJDBC=new UserDaoJDBCImpl();
-        userDaoJDBC.saveUser(name,lastName,age);
-        System.out.println(String.format("User с именем - %s добавлен в базу данных", name));
-    }
+public void saveUser(String name, String lastName, byte age) {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    userDaoJDBC.saveUser(name, lastName, age);
+    System.out.println(String.format("User с именем - %s добавлен в базу данных", name));
+}
 
-    public void removeUserById(long id) {
-        UserDaoJDBCImpl userDaoJDBC=new UserDaoJDBCImpl();
-        userDaoJDBC.removeUserById(id);
-    }
+public void removeUserById(long id) {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    userDaoJDBC.removeUserById(id);
+}
 
-    public List<User> getAllUsers() {
-        UserDaoJDBCImpl userDaoJDBC=new UserDaoJDBCImpl();
-        return userDaoJDBC.getAllUsers();
-    }
+public List<User> getAllUsers() {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    return userDaoJDBC.getAllUsers();
+}
 
-    public void cleanUsersTable() {
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.cleanUsersTable();
-    }
+public void cleanUsersTable() {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    userDaoJDBC.cleanUsersTable();
+}
 }
